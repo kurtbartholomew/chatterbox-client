ff2186ceeb1fc5438b7089a56116a4e286aaf659
kurtbartholomew <siever246@gmail.com>
2015-04-07 14:25:37 -0700

    Finished original implementation

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,3 @@
-// TODO: NEED TO place rooms in a select box.
-// Selecting join will call app.init()
-// Need to connect display messages and getnewmessages
-// with the refactored settings
-
 window.lastAddedAt = 0;
 window.intervalExists = undefined;
 window.chatWindow = $('#chats');
@@ -23,15 +18,35 @@ var sanitizeData = function(inputText) {
 
 var app = {};
 
+app.getNewMessages = function(room){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: {where:{'createdAt':{"$gt":lastAddedAt},'roomname':room}},
+    contentType: 'application/json',
+    success: function (data) {
+      if(data.results.length > 0) {
+        window.lastAddedAt = data.results[data.results.length-1].createdAt;
+        displayMessage(data.results, true);
+        if(chatWindow.children().length > 20){
+          chatWindow.find(".chat:nth-child(-n+" + data.results.length +")").remove();
+        }
+      }
+      if($('#roomSelect option:selected').val() === room) {
+        app.getNewMessages(room);
+      }
+    },
+    error: function(data) {
+      console.error('chatterbox: Message NOT retrieved');
+    }
+  });
+};
+
 app.joinRoom = function(room){
-  console.log(room);
   app.clearMessages();
   $('#send').show();
 
-  app.fetch({where:{'roomname':room}}, { recurring:true });
-
-//setInterval( function(){       })
-
+  app.fetch({where:{'roomname': room}}, { initial: true });
 };
 
 app.init = function(){
@@ -49,19 +64,23 @@ app.init = function(){
     app.addFriend(e.target);
   });
 
-
-
   // add an onsubmit handler to handle chat message submission
   $("#send").submit(function(e){
     e.preventDefault();
     app.handleSubmit();
   });
 
+  $(".room-create").on('click', function(e){
 
-  // **************************** Added recently
-   app.joinRoom($('#roomSelect option:selected').val());
-  // setInterval( function() { app.fetch({where:{'roomname':$('#roomSelect option:selected').val()}}, { recurring:true }); }, 10000);
+    var $roomName = $('#room-input');
+    app.addRoom($roomName.val());
 
+    $roomName.val('');
+    e.preventDefault();
+
+  });
+
+   app.joinRoom('lobby');
 };
 
 app.send = function(message){
@@ -71,7 +90,6 @@ app.send = function(message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
@@ -87,8 +105,13 @@ app.fetch = function(options, displayOptions){
     data: options,
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Messages retrieved');
-      displayMessage(data.results, displayOptions );
+      if(data.results.length > 0){
+        window.lastAddedAt = data.results[data.results.length-1].createdAt;
+        displayMessage(data.results, displayOptions );
+        if(options.where.roomname){
+          app.getNewMessages(options.where.roomname);
+        }
+      }
     },
     error: function(data) {
       console.error('chatterbox: Messages NOT retrieved');
@@ -113,7 +136,6 @@ app.addRoom = function(roomName) {
 };
 
 app.addFriend = function(domNode) {
-  console.log(domNode);
   var username = $(domNode).data('username');
   friends[username] = username;
 };
@@ -126,7 +148,6 @@ app.handleSubmit = function() {
     'roomname': $('#roomSelect option:selected').val()
   };
   $currentMessage.val('');
-  console.log(message);
   app.send(message);
 };
 
@@ -137,7 +158,6 @@ app.getRooms = function() {
     data: {limit:100, order:'-createdAt', keys:'roomname'},
     contentType: 'application/json',
     success: function (data) {
-      console.log('Got Rooms');
       app.populateRooms(data.results);
     },
     error: function(data) {
@@ -163,136 +183,18 @@ app.populateRooms = function(roomData) {
   });
 };
 
-app.init();
-
-//////////////////////////////////////////////////////////////
-
 var displayMessage = function(data, displayOptions) {
   _.each(data, function(messageObj) {
     var $msg = $('<div class="chat"><div class="username"><a href="" data-username="'+
     sanitizeData(messageObj.username)+'">' + sanitizeData(messageObj.username) + '</a></div>' +
   '<div class="message">'+ sanitizeData(messageObj.text) + '</div>' +
   '</div>');
-    if(displayOptions.recurring) {
-      // append and remove old
-      //
-      // call
-      chatWindow.append($msg);
-    } else {
+    if(displayOptions.initial) {
       chatWindow.prepend($msg);
+    } else {
+      chatWindow.append($msg);
     }
   });
 };
 
-// var sendMessage = function(message){
-//   $.ajax({
-//     // always use this url
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'POST',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//       console.log('chatterbox: Message sent');
-//     },
-//     error: function (data) {
-//       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//       console.error('chatterbox: Failed to send message');
-//     }
-//   });
-// };
-
-// var getMessages = function(){
-//   $.ajax({
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'GET',
-//     data: {limit:20,order:'-createdAt'},
-//     contentType: 'application/json',
-//     success: function (data) {
-//       window.lastAddedAt = data.results[data.results.length-1].createdAt;
-//       displayMessage(data.results, false);
-//       console.log(data);
-//       console.log('chatterbox: Message retrieved');
-//       if(!intervalExists){
-//         window.intervalExists = setInterval(getNewMessages,1000);
-//       }
-//     },
-//     error: function(data) {
-//       console.error('chatterbox: Message NOT retrieved');
-//     }
-//   });
-// };
-
-// var getNewMessages = function(){
-//   $.ajax({
-//     url: 'https://api.parse.com/1/classes/chatterbox',
-//     type: 'GET',
-//     data: {where:{'createdAt':{"$gt":lastAddedAt}}},
-//     contentType: 'application/json',
-//     success: function (data) {
-//       if(data.results.length > 0) {
-//         window.lastAddedAt = data.results[data.results.length-1].createdAt;
-//         displayMessage(data.results, true);
-//         chatWindow.find(".chat:nth-child(-n+" + data.results.length +")").remove();
-//         console.log('chatterbox: Message retrieved');
-//       }
-//     },
-//     error: function(data) {
-//       console.error('chatterbox: Message NOT retrieved');
-//     }
-//   });
-// };
-
-app.getRooms = function() {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    data: {limit:100, order:'-createdAt', keys:'roomname'},
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('Got Rooms');
-      populateRooms(data.results);
-    },
-    error: function(data) {
-      console.error('chatterbox: Message NOT retrieved');
-    }
-  });
-};
-
-app.populateRooms = function(roomData) {
-
-  var roomsD = roomData.map(function(object){
-    return object.roomname;
-  });
-  for(var i = 0; i < roomsD.length; i++) {
-    if(roomsD[i] !== undefined  && roomsD[i] !== null  && roomsD[i] !== "" && roomsD[i].indexOf('script') < 0){
-      var currentRoom =  sanitizeData(roomsD[i]);
-      rooms[currentRoom] = currentRoom;
-    }
-  }
-  _.each(rooms,function(room){
-    app.addRoom(room);
-  });
-};
-
-
-// $(".getMsg").click(getMessages);
-// $(".sendMsg").click(sendMessage);
-
-// $("#input-form").submit(function(e){
-//   var message = {
-//     'username': "fsdfdsfds",
-//     'text': document.getElementsByTagName('input')[0].value,
-//     'roomname': 'lobby'
-//   };
-//   document.getElementsByTagName('input')[0].value = "";
-//   console.log(message);
-//   sendMessage(message);
-//   e.preventDefault();
-// });
-
-// chatWindow.on('click','a', function(e){
-//   e.preventDefault();
-//   console.log(e.target);
-//   var username = $(e.target).data('username');
-//   friends[username] = username;
-// });
+app.init();
